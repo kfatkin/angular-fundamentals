1ce4bc9e2ab39dbcf6f0960a55f85a5a79490ede
kfatkin <kfatkin@gmail.com>
2020-08-18 21:48:36 -0400

    Components/Data Bindiings/Element References

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,15 @@
 import { Component } from "@angular/core";
 
+@Component({
+  selector: "app-root",
+  styleUrls: ["app.component.scss"],
+  templateUrl: "app.component.html",
+})
 export class AppComponent {
-  title: string = "Ultimate Angular";
+  name: string = "Kevin";
   constructor() {
+  }
+  handleClick(value: string) {
+    console.log(value);
   }
 }

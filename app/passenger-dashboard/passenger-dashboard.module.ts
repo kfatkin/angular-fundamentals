607cd5a2ee4a2a380f7cff7f67c2e207c77fcc57
kfatkin <kfatkin@gmail.com>
2020-08-21 22:54:16 -0400

    Link new components, handle event emitters

--- app/passenger-dashboard/passenger-dashboard.module.ts
+++ app/passenger-dashboard/passenger-dashboard.module.ts
@@ -2,9 +2,15 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 
 import { PassengerDashboardComponent } from "./containers/passenger-dashboard/passenger-dashboard.component";
+import { PassengerCountComponent } from "./componennts/passenger-count/passenger-count.component";
+import { PassengerDetailComponent } from "./componennts/passenger-detail/passenger-detail.component";
 
 @NgModule({
-  declarations: [PassengerDashboardComponent],
+  declarations: [
+    PassengerDashboardComponent,
+    PassengerCountComponent,
+    PassengerDetailComponent,
+  ],
   imports: [CommonModule],
   exports: [PassengerDashboardComponent],
 })

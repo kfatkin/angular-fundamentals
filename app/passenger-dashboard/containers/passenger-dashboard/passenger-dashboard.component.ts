906384d7974f770b66c610dc3d474e2ec07c8c40
kfatkin <kfatkin@gmail.com>
2020-08-23 12:07:16 -0400

    state change for removing and editing

--- app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -43,10 +43,18 @@ export class PassengerDashboardComponent implements OnInit {
       children: null,
     }];
   }
-  handleRemove(event) {
-    console.log(event);
+  handleRemove(event: Passenger) {
+    this.passengers = this.passengers.filter((passenger: Passenger) => {
+      return passenger.id !== event.id;
+    });
   }
-  handleEdit(event) {
-    console.log(event);
+  handleEdit(event: Passenger) {
+    this.passengers = this.passengers.map((passenger: Passenger) => {
+      if (passenger.id === event.id) {
+        passenger = Object.assign({}, passenger, event);
+      }
+      return passenger;
+    });
+    console.log(this.passengers);
   }
 }

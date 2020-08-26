7ae620b1043dcf40015f1395e6aa41bdc477e505
kfatkin <kfatkin@gmail.com>
2020-08-26 17:46:04 -0400

    Add in error handling

--- app/passenger-dashboard/passenger-dashboard.service.ts
+++ app/passenger-dashboard/passenger-dashboard.service.ts
@@ -3,6 +3,8 @@ import { Http, Response, Headers, RequestOptions } from "@angular/http";
 
 import { Observable } from "rxjs/Observable";
 import "rxjs/add/operator/map";
+import "rxjs/add/operator/catch";
+import "rxjs/add/observable/throw";
 
 import { Passenger } from "./models/passenger.interface";
 
@@ -15,7 +17,7 @@ export class PassengerDashboardService {
 
   getPassengers(): Observable<Passenger[]> {
     return this.http.get(PASSENGER_API).map((response: Response) =>
-      response.json()
+      response.json().catch((error: any) => Observable.throw(error.json()))
     );
   }
 
@@ -29,12 +31,14 @@ export class PassengerDashboardService {
     return this.http.put(`${PASSENGER_API}/${passenger.id}`, passenger, options)
       .map((
         response: Response,
-      ) => response.json());
+      ) =>
+        response.json().catch((error: any) => Observable.throw(error.json()))
+      );
   }
 
   removePassenger(passenger: Passenger): Observable<Passenger> {
     return this.http.delete(`${PASSENGER_API}/${passenger.id}`).map((
       response: Response,
-    ) => response.json());
+    ) => response.json().catch((error: any) => Observable.throw(error.json())));
   }
 }

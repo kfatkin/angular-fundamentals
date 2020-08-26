21008b1a6c724655749ee4b5a342d9251481d08b
kfatkin <kfatkin@gmail.com>
2020-08-26 17:36:37 -0400

    Add custom headers example

--- app/passenger-dashboard/passenger-dashboard.service.ts
+++ app/passenger-dashboard/passenger-dashboard.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { Http, Response } from "@angular/http";
+import { Http, Response, Headers, RequestOptions } from "@angular/http";
 
 import { Observable } from "rxjs/Observable";
 import "rxjs/add/operator/map";
@@ -20,9 +20,16 @@ export class PassengerDashboardService {
   }
 
   updatePassenger(passenger: Passenger): Observable<Passenger> {
-    return this.http.put(`${PASSENGER_API}/${passenger.id}`, passenger).map((
-      response: Response,
-    ) => response.json());
+    let headers = new Headers({
+      "Content-Type": "application/json",
+    });
+    let options = new RequestOptions({
+      headers: headers,
+    });
+    return this.http.put(`${PASSENGER_API}/${passenger.id}`, passenger, options)
+      .map((
+        response: Response,
+      ) => response.json());
   }
 
   removePassenger(passenger: Passenger): Observable<Passenger> {
